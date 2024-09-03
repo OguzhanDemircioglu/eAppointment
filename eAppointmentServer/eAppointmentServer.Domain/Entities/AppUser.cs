29760d65a55606463d6916c5ee05b033a5904aad
OguzhanMonster <oguzhanturgut611@gmail.com>
2024-09-03 10:17:03 +0300

    init

---
+++ eAppointmentServer/eAppointmentServer.Domain/Entities/AppUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace eAppointmentServer.Domain.Entities;
+
+public sealed class AppUser: IdentityUser<Guid>
+{
+    public required string FirstName { get; set; }
+    public required string LastName { get; set; }
+    public string FullName => string.Join(" ", FirstName, LastName);
+}
---
+++ eAppointmentServer/eAppointmentServer.Domain/Entities/Doctor.cs
@@ -0,0 +1,12 @@
+﻿using eAppointmentServer.Domain.Abstractions;
+using eAppointmentServer.Domain.Enums;
+
+namespace eAppointmentServer.Domain.Entities;
+
+public sealed class Doctor: Entity
+{
+    public string FirstName { get; set; } = String.Empty;
+    public string LastName { get; set; } = String.Empty;
+    public string FullName => string.Join(" ", FirstName, LastName);
+    public DepartmentEnum Department { get; set; } = DepartmentEnum.Acil;
+}
